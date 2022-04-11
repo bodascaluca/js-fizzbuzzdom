739c49d85576878101fa487d4e0602b222b41e07
bodascaluca <bodasca.luca@gmail.com>
2022-04-11 17:01:27 +0200

    Add script in square

--- Js/script.js
+++ Js/script.js
@@ -12,12 +12,14 @@ for(let i = 1; i<=100; i++){
         oddOrEven ="fizzbuzz";
     } else if(i % 3 === 0){
         oddOrEven = "fizz";
-    }else if (i % 5 === 0) {
+    } else if (i % 5 === 0) {
         oddOrEven = "buzz";
-    } 
+    } else {
+        oddOrEven = i;
+    }
 
 
     // 2 OUTPUT
-    rowWrapper.innerHTML += `<div class="box ${oddOrEven} "> ${i}</div>`;
+    rowWrapper.innerHTML += `<div class="box ${oddOrEven} "> ${oddOrEven}</div>`;
     // console.log(i,oddOrEven);
 }
