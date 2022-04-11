32ecce9eac3170988fdeb9b02ac3fa82a3578605
bodascaluca <bodasca.luca@gmail.com>
2022-04-11 16:16:19 +0200

    Add fizz/buzz/fizzbuzz

---
+++ Js/script.js
@@ -0,0 +1,23 @@
+
+// colorare i numeri in base se è pari o dispari
+const rowWrapper = document.querySelector(".row");
+
+// Stampa nella console
+for(let i = 1; i<=100; i++){
+
+    // Deffinire se è pari o dispari 
+    let oddOrEven;
+
+    if (i % 15 === 0){
+        oddOrEven ="fizzbuzz";
+    } else if(i % 3 === 0){
+        oddOrEven = "fizz";
+    }else if (i % 5 === 0) {
+        oddOrEven = "buzz";
+    } 
+
+
+    // 2 OUTPUT
+    rowWrapper.innerHTML += `<div class="box ${oddOrEven} "> ${i}</div>`;
+    // console.log(i,oddOrEven);
+}
